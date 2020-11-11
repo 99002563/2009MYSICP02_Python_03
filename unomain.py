962dd7fc7120108bed8ffe590a62d9bb281b4638
99002563 <71481192+99002563@users.noreply.github.com>
2020-11-11 14:41:34 +0530

    Update unomain.py

--- unomain.py
+++ unomain.py
@@ -51,7 +51,36 @@ class Deck:
         
     def deal(self):
         return self.deck.pop()
-                
+   
+class Hand:
+    
+    def __init__(self):
+        self.cards = []
+        self.cardsstr = []
+        self.number_cards = 0
+        self.action_cards = 0
+    
+    def add_card(self,card):
+        self.cards.append(card)
+        self.cardsstr.append(str(card))
+        if card.cardtype=='number':
+            self.number_cards+=1
+        else:
+            self.action_cards+=1
+            
+    def remove_card(self,place):
+        self.cardsstr.pop(place-1)
+        return self.cards.pop(place-1)
+    
+    def cards_in_hand(self):
+        for i in range(len(self.cardsstr)):
+            print(f' {i+1}.{self.cardsstr[i]}' )
+            
+    def single_card(self,place):
+        return self.cards[place-1]
+    
+    def no_of_cards(self):
+        return len(self.cards)
                 
  #Funciton to randomly select who starts first
 def choose_first():
@@ -67,9 +96,6 @@ def single_card_check(top_card,card):
     else:
         return False
 
-    
-
-=======
 
 #FOR PC ONLY
 #To check if PC has any valid card to throw 
@@ -87,7 +113,7 @@ def win_check(hand):
     else:
         return False
       
- main
+ 
 #The gaming loop
 while True:
 
